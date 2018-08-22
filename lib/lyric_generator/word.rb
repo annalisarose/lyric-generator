77a0e732ba3fc72bbecae9890877739d675969fa
Annalisa <arosegraz@gmail.com>
2018-08-21 21:47:59 -0700

    create word class and word.create_table method

---
+++ lib/lyric_generator/word.rb
@@ -0,0 +1,15 @@
+class LyricGenerator::Word
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS words (
+        id INTEGER PRIMARY KEY,
+        noun TEXT,
+        adjective TEXT,
+        verb TEXT,
+        adverb TEXT
+      )
+    SQL
+  end
+
+end
