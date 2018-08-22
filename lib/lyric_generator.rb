520b359451146bb3d96c13a9e1c5fe28b6db15a0
Annalisa <arosegraz@gmail.com>
2018-08-21 21:54:19 -0700

    add dependencies for bundler and sqlite3, set DB var in environment file. mkdir db and add words.db

--- lib/lyric_generator.rb
+++ lib/lyric_generator.rb
@@ -1,3 +1,9 @@
 #environment file
+require 'bundler'
+require 'SQLite3'
+Bundler.require
+
+DB = {:conn => SQLite3::Database.new("db/words.db")}
+
 require_relative "./lyric_generator/version"
 require_relative "./lyric_generator/cli"
